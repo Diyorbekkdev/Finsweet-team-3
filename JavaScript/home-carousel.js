2d607ca3a708f0cb1dfa7ee5e0bf0e0522462258
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-05-15 13:38:34 +0500

    the following sections

--- JavaScript/home-carousel.js
+++ JavaScript/home-carousel.js
@@ -13,3 +13,21 @@ $('.owl-carousel').owlCarousel({
         },
     }
 })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
